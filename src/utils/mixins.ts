28635204eea6ce518c4aca7f1c19ff2ea78cb778
José Luis Cantos <jose.cantos@epc-tracker.com>
2021-02-22 18:56:55 +0100

    Remove unused functiosn on mixins

--- src/utils/mixins.ts
+++ src/utils/mixins.ts
@@ -1,9 +1,3 @@
-import {Dimensions, PixelRatio} from 'react-native';
-
-const WINDOW_WIDTH = Dimensions.get('window').width;
-const guidelineBaseWidth = 375;
-
-export const scaleSize = (size: number) =>
-  (WINDOW_WIDTH / guidelineBaseWidth) * size;
+import { PixelRatio } from 'react-native';
 
 export const scaleFont = (size: number) => size * PixelRatio.getFontScale();
