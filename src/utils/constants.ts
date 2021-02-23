28fa3a12e205449d3e84ee4dafb418c4a76541d5
José Luis Cantos <jose.cantos@epc-tracker.com>
2021-02-23 17:15:08 +0100

    Modify file constants for include tabs_route_name

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,30 +2,35 @@ export const API_URL = 'https://stores.robinrover.io/api';
 export const API_RESULTS_PER_PAGE = 10;
 export const TAB_BAR_ICON_SIZE = 24;
 
+export enum TABS_ROUTE_NAME {
+  SHOPS = 'ShopsList',
+  FAVORITES = 'ShopsFavoriteList'
+}
+
+export const TAB_SHOPS_LIST = 'ShopsList';
+
 export const Colors = {      
   // MAIN COLORS
-  main:   {
-  PRIMARY : '#1B9BD7',
-  SECONDARY : '#f4511e',  
+  main: {
+    PRIMARY : '#1B9BD7',
+    SECONDARY : '#f4511e',  
   },
   // GRAYSCALE
   grayscale:   {
-  BLACK : '#000000',
-/*    GRAY_LIGHT : '#e6e6e6', */
-  GRAY_LIGHT : '#F6F6F6',
-  GRAY_MEDIUM : '#cacaca',
-  GRAY_DARK : '#8a8a8a',
-  GRAY_TYPO : '#888686',
-  WHITE : '#ffffff',
+    BLACK : '#000000',
+    GRAY_LIGHT : '#F6F6F6',
+    GRAY_MEDIUM : '#cacaca',
+    GRAY_DARK : '#8a8a8a',
+    GRAY_TYPO : '#888686',
+    WHITE : '#ffffff',
   },
   actions:   {
-  // ACTIONS
-  SUCCESS : '#47A665',
-  WARNING : '#ffae00',
-  ALERT : '#D24858',
-  ALERT_TRANSPARENT : '#FFECEC',
-  WARNING_TRANSPARENT: '#ffd780',
-  SUCCESS_TRANSPARENT: '#94d1a7',
-  YELLOW : '#F9C962',
+    SUCCESS : '#47A665',
+    WARNING : '#ffae00',
+    ALERT : '#D24858',
+    ALERT_TRANSPARENT : '#FFECEC',
+    WARNING_TRANSPARENT: '#ffd780',
+    SUCCESS_TRANSPARENT: '#94d1a7',
+    YELLOW : '#F9C962',
   },
 };
