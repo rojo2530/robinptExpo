7f00cfb8aeb039d32676b6c12b25e9d650b8054a
José Luis Cantos <jose.cantos@epc-tracker.com>
2021-02-23 17:50:14 +0100

    Add component Title

---
+++ src/components/title/title.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Colors } from '../../utils/constants';
+
+interface Props {
+  numberOfLines?: number,
+  color?: string,
+  fontSize?: number,
+}
+
+const Title: React.FunctionComponent<Props> = (props) => (
+  <Text
+  numberOfLines={props.numberOfLines}
+    style={{
+      fontSize: props.fontSize,
+      color: props.color
+    }}>
+    {props.children}
+  </Text>
+);
+
+Title.defaultProps = {
+  numberOfLines: 3,
+  color: Colors.grayscale.BLACK,
+  fontSize: 22
+}
+
+export default Title;
